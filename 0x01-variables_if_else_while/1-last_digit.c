d5806e811337a8063581870b11b80dd0010f2235
absheriff <abdulfataisheriffdeen@gmail.com>
2023-12-25 21:53:03 +0100

    C update

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+/*
+ * File: 1-last_digit.c
+ * Desc: It checkts if the last number of the randomly generated integers is < 5,  =0 or < 6 && > 0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main(void){
+	int n, last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX/2;
+
+	//the number of string generated is saved inside gstr;
+	char gstr[20];
+
+	sprintf(gstr, "%d", n);
+	last = gstr[strlen(gstr)-1];
+	last = last - '0';
+	
+	if(n<0) last = last * -1;
+
+	if(last > 5) 
+		printf("Last digit of %d is %d and is greater than 5", n, last);
+	else if(last == 0) 
+		printf("Last digit of %d is %d and is 0", n, last);
+	else if(last < 6 && last != 0) 
+		printf("Last digit of %d is %d  and is less than 6 and not 0", n, last);
+
+	printf("\n");
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,5 @@
+- 0-positive_or_negative.c: is a random number generator and stating if the generated number is 0, less than 0 or greater than 0
+
+- 1-last_digit.c: checks the last digit of the randomly generated integer and perform some operations
+
+- 
